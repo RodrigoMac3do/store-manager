1918166084e43874c48a9bf8de2d8fd791b3fcb7
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-12 11:49:52 -0300

    feat: faz a busca do produto por id

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -2,10 +2,21 @@ const connection = require('./connection');
 
 const listAll = async () => {
   const query = 'SELECT * FROM StoreManager.products';
+
   const [products] = await connection.execute(query);
+  
   return products;
+};
+
+const findById = async (id) => {
+  const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
+  
+  const [[product]] = await connection.execute(query, [id]);
+
+  return product;
 };
 
 module.exports = {
   listAll,
+  findById,
 };
