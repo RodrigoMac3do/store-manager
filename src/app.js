0d80e908b734191d6362710bada81b61eb1ecd62
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-11 23:07:17 -0300

    add: rota de products

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const router = require('./routers');
 
 const app = express();
+
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+app.use('/products', router.products);
+
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
+// você deve usar o arquivo index.js para executar sua aplicação
 module.exports = app;
