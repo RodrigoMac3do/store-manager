42f0f7b0a6827ac111ca31a64f88cda2099093f1
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-12 12:25:00 -0300

    feat: cria novo produto por nome  e traz insertId

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -4,19 +4,28 @@ const listAll = async () => {
   const query = 'SELECT * FROM StoreManager.products';
 
   const [products] = await connection.execute(query);
-  
+
   return products;
 };
 
 const findById = async (id) => {
   const query = 'SELECT * FROM StoreManager.products WHERE id = ?';
-  
+
   const [[product]] = await connection.execute(query, [id]);
 
   return product;
 };
 
+const insert = async (name) => {
+  const query = 'INSERT INTO StoreManager.products (name) VALUES (?)';
+
+  const [{ insertId }] = await connection.execute(query, [name]);
+
+  return insertId;
+};
+
 module.exports = {
   listAll,
   findById,
+  insert,
 };
