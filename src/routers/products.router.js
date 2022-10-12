175ae2ea73970d9b63a2deac6e03cc66d1122462
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-12 11:52:21 -0300

    feat: criação de rota para products/id

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -4,5 +4,6 @@ const controller = require('../controllers');
 const router = express.Router();
 
 router.get('/', controller.products.listAll);
+router.get('/:id', controller.products.findById);
 
 module.exports = router;
