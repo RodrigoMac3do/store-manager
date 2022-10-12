2271ff75cd4af104e5dc366ce4741f6efb3f05cf
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-12 12:55:15 -0300

    feat: rota de sale/id

--- src/routers/sales.router.js
+++ src/routers/sales.router.js
@@ -4,5 +4,6 @@ const controller = require('../controllers');
 const router = express.Router();
 
 router.get('/', controller.sales.listAll);
+router.get('/:id', controller.sales.findById);
 
 module.exports = router;
