386fbf4c0b8f1edbc5d10abf9e65cc6994115bec
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-12 12:20:46 -0300

    feat: rota para inserir produto

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.get('/', controller.products.listAll);
 router.get('/:id', controller.products.findById);
+router.post('/', controller.products.insert);
 
 module.exports = router;
