c70b1bb9102473a0e61f8640fc2fec2bacaee2ae
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-12 11:51:47 -0300

    feat: traz o produto/id e ou pega o erro

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,10 +2,23 @@ const service = require('../services');
 
 const listAll = async (_req, res, _next) => {
   const products = await service.products.listAll();
- 
+
   res.status(200).json(products);
+};
+
+const findById = async (req, res, next) => {
+  const id = Number(req.params.id);
+
+  try {
+    const product = await service.products.findById(id);
+
+    res.status(200).json(product);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
   listAll,
+  findById,
 };
