2f8436753eb2f50d44e358ba80f0977cc62d2e1f
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-12 12:55:39 -0300

    feat: busca sale por id

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -6,6 +6,19 @@ const listAll = async (_req, res, _next) => {
   res.status(200).json(sales);
 };
 
+const findById = async (req, res, next) => {
+  const id = Number(req.params.id);
+
+  try {
+    const sale = await service.sales.findById(id);
+
+    res.status(200).json(sale);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   listAll,
+  findById,
 };
