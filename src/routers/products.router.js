a7b2b0737eb01866b9347a39d4e2f9273245bbea
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-12 12:30:21 -0300

    add: middleware de name na rota post

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const controller = require('../controllers');
+const middleware = require('../middlewares');
 
 const router = express.Router();
 
 router.get('/', controller.products.listAll);
 router.get('/:id', controller.products.findById);
-router.post('/', controller.products.insert);
+router.post('/', middleware.name, controller.products.insert);
 
 module.exports = router;
