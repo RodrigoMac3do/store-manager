9e661e2fa3daa9d86e1aa4fff4aecd2f1192505b
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-13 17:41:12 -0300

    fix: muda if para lançar erro no array vazio

--- src/services/sales.service.js
+++ src/services/sales.service.js
@@ -3,14 +3,14 @@ const model = require('../models');
 
 const listAll = async () => {
   const sales = await model.sales.listAll();
-  
+
   return sales;
 };
 
 const findById = async (id) => {
   const sale = await model.sales.findById(id);
 
-  if (sale === undefined) {
+  if (sale.length === 0) {
     throw sendError(404, 'Sale not found');
   }
 
