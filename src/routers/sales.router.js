537868685bf799b554ce4db4642707a6b7d5daca
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-14 21:07:56 -0300

    Revert "feat: rota de post com middlewares"
    This reverts commit cb193302fb8e30eaaeeb13d3c79b68ea91d37054.

--- src/routers/sales.router.js
+++ src/routers/sales.router.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const controller = require('../controllers');
-const middleware = require('../middlewares');
 
 const router = express.Router();
 
 router.get('/', controller.sales.listAll);
 router.get('/:id', controller.sales.findById);
-router.post(
-  '/',
-  middleware.quantity,
-  middleware.product,
-  controller.sales.insert,
-);
 
 module.exports = router;
