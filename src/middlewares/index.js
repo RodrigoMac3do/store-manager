9d89f76c1ea0810a8763cd74b2683d0a3e5b0aa4
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-14 23:41:19 -0300

    add: middleware de name para put

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,9 +1,11 @@
 const name = require('./validationName');
 const product = require('./validationProduct');
 const quantity = require('./validationQuantity');
+const nameProducts = require('./validationProductName');
 
 module.exports = {
   name,
   product,
   quantity,
+  nameProducts,
 };
