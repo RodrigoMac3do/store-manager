3dd7a409e26e13d424a13130645177ebed9428df
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-14 21:42:01 -0300

    add: middlewares de product e quantity

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,9 @@
 const name = require('./validationName');
+const product = require('./validationProduct');
+const quantity = require('./validationQuantity');
 
 module.exports = {
   name,
+  product,
+  quantity,
 };
