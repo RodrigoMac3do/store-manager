8355869133a34f7adf12d021299e88a0ff7ecd92
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-18 17:48:39 -0300

    feat: Teste de para produto id na service

--- tests/unit/services/products.service.test.js
+++ tests/unit/services/products.service.test.js
@@ -13,6 +13,20 @@ describe("Testes de unidade do service de produtos", () => {
 
       expect(result).to.be.deep.equal(allProductsResponse);
     });
+    it("Listar produto por id com sucesso", async () => {
+      sinon.stub(model.products, "findById").resolves(allProductsResponse);
+
+      const result = await service.products.findById(1);
+
+      expect(result).to.be.deep.equal(allProductsResponse[0]);
+    });
+    it("Listar produto por id sem sucesso", async () => {
+      sinon.stub(model.products, "findById").resolves([]);
+
+      const result = await service.products.findById(0);
+     
+      expect(result).to.be.deep.equal([]);
+    });
     beforeEach(sinon.restore);
   });
 });
