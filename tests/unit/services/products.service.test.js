ba5e27c31d081dfe6053547b07d543b771aea302
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-17 22:36:46 -0300

    feat: teste da service listagem de todos produtos

---
+++ tests/unit/services/products.service.test.js
@@ -0,0 +1,18 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const model = require("../../../src/models");
+const service = require("../../../src/services");
+const { allProductsResponse } = require("../mocks");
+
+describe("Testes de unidade do service de produtos", () => {
+  describe("Listar todos os produtos", () => {
+    it("Listar todos os produtos com sucesso", async () => {
+      sinon.stub(model.products, "listAll").resolves(allProductsResponse);
+
+      const result = await service.products.listAll();
+
+      expect(result).to.be.deep.equal(allProductsResponse);
+    });
+    beforeEach(sinon.restore);
+  });
+});
