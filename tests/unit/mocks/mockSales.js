145728f04ea2ec507bb606fae0e3102643ce6f40
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-10-18 18:10:10 -0300

    feat: mock para sales

---
+++ tests/unit/mocks/mockSales.js
@@ -0,0 +1,59 @@
+/*-------------------------- Sales --------------------------*/
+const wrongSaleNotProductIdBody = [{ quantity: 1 }];
+const wrongSaleNotQuantityBody = [{ productId: 1 }];
+const nonexistentProductIdBody = [{ productId: 9999, quantity: 1 }];
+const nonexistentProductIdBody2 = [
+  { productId: 1, quantity: 1 },
+  { productId: 99999, quantity: 5 },
+];
+const wrongZeroQuantityBody = [{ productId: 1, quantity: 0 }];
+const wrongZeroNegativeBody = [{ productId: 1, quantity: -1 }];
+const otherProductIdSaleBody = [
+  { productId: 1, quantity: 1 },
+  { productId: 3, quantity: 5 },
+];
+const rightSaleBody = [
+  { productId: 1, quantity: 1 },
+  { productId: 2, quantity: 5 },
+];
+const saleCreateResponse = {
+  id: 3,
+  itemsSold: [
+    { productId: 1, quantity: 1 },
+    { productId: 2, quantity: 5 },
+  ],
+};
+
+const allSales = [
+  {
+    saleId: 1,
+    date: "2022-10-18T20:57:27.000Z",
+    productId: 1,
+    quantity: 5,
+  },
+  {
+    saleId: 1,
+    date: "2022-10-18T20:57:27.000Z",
+    productId: 2,
+    quantity: 10,
+  },
+  {
+    saleId: 2,
+    date: "2022-10-18T20:57:27.000Z",
+    productId: 3,
+    quantity: 15,
+  },
+];
+
+module.exports = {
+  wrongSaleNotProductIdBody,
+  wrongSaleNotQuantityBody,
+  nonexistentProductIdBody,
+  nonexistentProductIdBody2,
+  wrongZeroQuantityBody,
+  wrongZeroNegativeBody,
+  otherProductIdSaleBody,
+  rightSaleBody,
+  saleCreateResponse,
+  allSales,
+};
