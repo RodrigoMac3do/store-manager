6d7bb4e6917cab16709c4dc4686d11582993b053
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-04 16:38:33 -0300

    refactor: muda validacao para service

--- src/middlewares/validationProductName.js
+++ src/middlewares/validationProductName.js
@@ -1,15 +1,8 @@
-const model = require('../models');
-
 const nameProducts = async (req, res, next) => {
   const { name } = req.body;
-  const id = Number(req.params.id);
-
-  const productIdDB = await model.products.findById(id);
 
   if (!name) {
     res.status(400).json({ message: '"name" is required' });
-  } else if (productIdDB.length === 0) {
-    res.status(404).json({ message: 'Product not found' });
   } else if (name.length < 5) {
     res
       .status(422)
