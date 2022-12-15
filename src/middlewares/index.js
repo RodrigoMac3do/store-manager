612506da90e637380bd0bd0c20e40b7bf1a5dec5
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-12-15 00:18:26 -0300

    add: import e export de middleware de erro

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -2,8 +2,10 @@ const name = require('./validationName');
 const product = require('./validationProduct');
 const quantity = require('./validationQuantity');
 const nameProducts = require('./validationProductName');
+const httpError = require('./http.error.middleware');
 
 module.exports = {
+  httpError,
   name,
   product,
   quantity,
