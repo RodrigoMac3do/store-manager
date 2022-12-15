6c0c3bfecb27c55836bd83a3ae1319069f4a0726
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-12-15 09:34:53 -0300

    feat: schema para sale e tamanho de name

--- src/services/validations/schema.js
+++ src/services/validations/schema.js
@@ -1,9 +1,15 @@
 const Joi = require('joi');
 
 const productSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().min(5).required(),
+});
+
+const saleSchema = Joi.object({
+  productId: Joi.number().required(),
+  quantity: Joi.number().required(),
 });
 
 module.exports = {
   productSchema,
+  saleSchema,
 };
