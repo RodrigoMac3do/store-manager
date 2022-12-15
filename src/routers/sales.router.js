6f1c4daa8559dfbe971ba8106096e75e44ff6634
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-12-15 10:58:20 -0300

    refactor: remove middlewares e ajusta espaçamento

--- src/routers/sales.router.js
+++ src/routers/sales.router.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const controller = require('../controllers');
-const middleware = require('../middlewares');
 
 const router = express.Router();
 
 router.get('/', controller.sales.listAll);
+
 router.get('/:id', controller.sales.findById);
-router.post(
-  '/',
-  middleware.quantity,
-  middleware.product.keyProduct,
-  middleware.product.valuesProduct,
-  controller.sales.insert,
-);
+
+router.post('/', controller.sales.insert);
+
+router.put('/:id', controller.sales.updateById);
+
 router.delete('/:id', controller.sales.remove);
 
 module.exports = router;
