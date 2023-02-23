02c55944f6ecdb0bdd571300d1411615e26b9170
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-23 15:38:35 -0300

    fix: import e export de middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,13 +1,5 @@
-const name = require('./validationName');
-const product = require('./validationProduct');
-const quantity = require('./validationQuantity');
-const nameProducts = require('./validationProductName');
 const httpErrorMiddleware = require('./http.error.middleware');
 
 module.exports = {
   httpErrorMiddleware,
-  name,
-  product,
-  quantity,
-  nameProducts,
 };
