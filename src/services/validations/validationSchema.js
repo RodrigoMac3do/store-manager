572d69a692d19af7233a36a59b3f45b7bbe22537
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-23 15:40:53 -0300

    refactor: validador de schema

--- src/services/validations/validationSchema.js
+++ src/services/validations/validationSchema.js
@@ -1,14 +1,11 @@
 const httpException = require('../../utils/http.exception');
-const { mapStatusCode, mapMessage } = require('./statusCode');
+const { mapStatusCode } = require('./statusCode');
 
 const validateSchema = (schema, data) => {
   const { error, value } = schema.validate(data);
 
   if (error) {
-    throw httpException(
-      mapStatusCode(error.message),
-      mapMessage(error.message),
-    );
+    throw httpException(mapStatusCode(error.message), error.message);
   }
 
   return value;
