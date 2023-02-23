bd19531fd3815a77b34e49b56fdf12224c2bb3ba
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-23 15:42:49 -0300

    style: formatação do prettier

--- seed.sql
+++ seed.sql
@@ -1,23 +1,33 @@
 USE `StoreManager`;
 
-SET FOREIGN_KEY_CHECKS = 0;
+SET
+  FOREIGN_KEY_CHECKS = 0;
 
 TRUNCATE TABLE `products`;
+
 TRUNCATE TABLE `sales`;
+
 TRUNCATE TABLE `sales_products`;
 
-SET FOREIGN_KEY_CHECKS = 1;
+SET
+  FOREIGN_KEY_CHECKS = 1;
 
-INSERT INTO StoreManager.products (name) VALUES
-    ("Martelo de Thor"),
-    ("Traje de encolhimento"),
-    ("Escudo do Capitão América");
+INSERT INTO
+  StoreManager.products (name)
+VALUES
+  ("Martelo de Thor"),
+  ("Traje de encolhimento"),
+  ("Escudo do Capitão América");
 
-INSERT INTO StoreManager.sales (date) VALUES
-    (NOW()),
-    (NOW());
+INSERT INTO
+  StoreManager.sales (date)
+VALUES
+  (NOW()),
+  (NOW());
 
-INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES
-    (1, 1, 5),
-    (1, 2, 10),
-    (2, 3, 15);
+INSERT INTO
+  StoreManager.sales_products (sale_id, product_id, quantity)
+VALUES
+  (1, 1, 5),
+  (1, 2, 10),
+  (2, 3, 15);
